f918f6e81638e8cb69b55ae5e6b4c451acccd8be
= <=>
2022-03-27 19:54:28 +0300

    first commit

---
+++ src/main/java/com/example/testgitcdci/TestGitCdCiApplication.java
@@ -0,0 +1,21 @@
+package com.example.testgitcdci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class TestGitCdCiApplication {
+
+    @GetMapping
+    public String getHello() {
+        return "Hello application !!!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGitCdCiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testgitcdci/TestGitCdCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testgitcdci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGitCdCiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
