d55a84be28577e2d7e69ae01f27cc80808bbc1ae
= <=>
2022-03-29 00:50:35 +0300

    add tests

--- src/test/java/com/example/testgitcdci/TestGitCdCiApplicationTests.java
+++ src/test/java/com/example/testgitcdci/TestGitCdCiApplicationTests.java
@@ -3,11 +3,32 @@ package com.example.testgitcdci;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 @SpringBootTest
 class TestGitCdCiApplicationTests {
 
+    Calculator calculator = new Calculator();
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void badTest() {
+        int a = 20;
+        int b = 30;
+
+        int result = calculator.calculate(a, b);
+
+        assertThat(result).isEqualTo(50);
+    }
+
+    class Calculator {
+        int calculate(int a, int b) {
+            return a + b;
+        }
     }
 
 }
